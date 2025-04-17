89f1af9279e1c47c7aedde8c00e65eb2ffc8da45
Ubuntu <ubuntu@ip-172-31-80-209.ec2.internal>
2025-04-17 02:24:43 +0000

    edit dataloader

--- finetune.py
+++ finetune.py
@@ -30,7 +30,7 @@ assert MODEL_DEPTH in {16, 20, 24, 30, 36}
 
 # download checkpoint
 # hf_home = 'https://huggingface.co/FoundationVision/var/resolve/main'
-vae_ckpt, var_ckpt = './var_d36.pth', f'./var_d36.pth'
+vae_ckpt, var_ckpt = './vae_ch160v4096z32.pth', f'./var_d36.pth'
 # if not osp.exists(vae_ckpt): os.system(f'wget {hf_home}/{vae_ckpt}')
 # if not osp.exists(var_ckpt): os.system(f'wget {hf_home}/{var_ckpt}')
 
@@ -152,6 +152,6 @@ def train_var_for_sr(var, vae, dataloader, device='cuda', epochs=10, lr=1e-4):
 
 
 # === Usage Example ===
-dataset = Flickr2KSRDataset(root_dir='./data/kagglehub/datasets/daehoyang/flickr2k/versions', scale=2, patch_size=512, stride=256)
+dataset = Flickr2KSRDataset(root_dir='./data/kagglehub/datasets/daehoyang/flickr2k/versions/1/Flickr2K', scale=2, patch_size=512, stride=256)
 dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
 train_var_for_sr(var, vae, dataloader, device='cuda', epochs=5)
